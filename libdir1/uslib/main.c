41ee660bc39ba004034592053f62973729a761ed
SakuyaRemilya <1184185429@qq.com>
2024-04-10 19:25:24 +0800

    动静态库练习

---
+++ libdir1/uslib/main.c
@@ -0,0 +1,14 @@
+#include "mymath.h"
+#include "myprint.h"
+
+int main()
+{
+    int sum =0;
+    sum = Add(100,250);
+    printf("ADD res:%d\n",sum);
+    printf("ADD res:%d\n",sum);
+    printf("ADD res:%d\n",sum);
+    printf("ADD res:%d\n",sum);
+    Print();
+    return 0;
+}
